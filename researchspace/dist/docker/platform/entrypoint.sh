f7847842f4bca5b8121aee5572df10b9cb6902b8
Frank Sachsenheim <funkyfuture@riseup.net>
2019-04-29 23:10:18 +0200

    Uses explicit user & group names in platform/entrypint.sh

--- researchspace/dist/docker/platform/entrypoint.sh
+++ researchspace/dist/docker/platform/entrypoint.sh
@@ -1,9 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 
 set -e
 
-chown -R 100:101 /apps
-chown -R 100:101 /data
-chown -R 100:101 /config
+chown -R jetty:jetty /apps /config /data
 
 exec java $JAVA_OPTS -jar -Djava.io.tmpdir=$TMPDIR $JETTY_HOME/start.jar /usr/local/jetty/etc/jetty.xml /usr/local/jetty/etc/jetty-http-forwarded.xml $RUNTIME_OPTS $PLATFORM_OPTS
